249d007b4810c07a02e1fa6a722131818919ad4e
FrankCostelliJr <frank.costelli@outlook.com>
2021-02-15 17:18:55 -0500

    code added for add card screen

--- src/Layout/Cards/AddCard.js
+++ src/Layout/Cards/AddCard.js
@@ -1,8 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { createCard, readDeck } from '../../utils/api/index';
+
 
 function AddCard() {
+  const initialState = { 
+    front: '',
+    back: ''
+  };
+
+  const [formData, setFormData] = useState(initialState);
+  const [deck, setDeck] = useState({});
+  const { deckId } = useParams();
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData, 
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await createCard(deckId, formData);
+    setFormData(initialState);
+  };
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const loadDeck = async () => {
+      const loadedDeck = await readDeck(deckId, abortController.signal);
+      setDeck(() => loadedDeck);
+    };
+    loadDeck();
+    return () => abortController.abort();
+  }, [deckId]);
+
+
   return (
     <div>
+      <nav aria-label='breadcrumb'>
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item text-primary">
+            <Link to='/'>Home</Link>
+          </li>
+          <li className="breadcrumb-item text-primary">
+            <Link to={`/decks/${deckId}`}>{deck.name}</Link>
+          </li>
+          <li className="breadcrumb-item active" aria-current='page'>
+            Add Card
+          </li>
+        </ol>
+      </nav>
+      <h1>{deck.name}: Add Cards</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="front"><h5>Front</h5></label>
+          <textarea
+            className="form-control" 
+            name="front" 
+            rows="3"
+            placeholder="Front side of card"
+            value={formData.front}
+            onChange={handleChange}
+            required
+          >
+          </textarea>
+          <label htmlFor="back"><h5>Back</h5></label>
+        </div>
+      </form>
       
     </div>
   )
